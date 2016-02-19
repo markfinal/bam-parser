0c92e9a54416981ee0e5aa255b9c2df0e39bb5c2
Mark Final <markfinal@hotmail.com>
2016-02-19 21:37:55 +0000

    [packages,flex,2.6.0] Build the flex executable

--- packages/flex-2.6.0/bam/Scripts/flex.cs
+++ packages/flex-2.6.0/bam/Scripts/flex.cs
@@ -1,5 +1,26 @@
 using Bam.Core;
 namespace flex
 {
-    // write modules here ...
+    sealed class flex :
+        C.ConsoleApplication
+    {
+        protected override void
+        Init(
+            Bam.Core.Module parent)
+        {
+            base.Init(parent);
+
+            this.CreateHeaderContainer("$(packagedir)/src/*.h");
+            var source = this.CreateCSourceContainer("$(packagedir)/src/*.c", filter: new System.Text.RegularExpressions.Regex(@"^((?!.*libmain)(?!.*libyywrap).*)$"));
+            source.PrivatePatch(settings =>
+                {
+                    var compiler = settings as C.ICommonCompilerSettings;
+                    compiler.PreprocessorDefines.Add("STDC_HEADERS");
+                    compiler.PreprocessorDefines.Add("HAVE_REGEX_H");
+                    compiler.PreprocessorDefines.Add("HAVE_LIMITS_H");
+                    compiler.PreprocessorDefines.Add("VERSION", "\"2.6.0\"");
+                    compiler.PreprocessorDefines.Add("M4", "m4");
+                });
+        }
+    }
 }
