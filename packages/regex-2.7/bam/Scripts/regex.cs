b7aeda34090bd044e3d74d24d5e13faba1bea613
Mark Final <markfinal@hotmail.com>
2016-02-20 22:46:42 +0000

    [packages,regex,2.7] Windows compilation rules

--- packages/regex-2.7/bam/Scripts/regex.cs
+++ packages/regex-2.7/bam/Scripts/regex.cs
@@ -1,5 +1,38 @@
 using Bam.Core;
 namespace regex
 {
-    // write modules here ...
+    sealed class regex :
+        C.StaticLibrary
+    {
+        protected override void
+        Init(
+            Bam.Core.Module parent)
+        {
+            base.Init(parent);
+
+            this.CreateHeaderContainer("$(packagedir)/src/regex/2.7/regex-2.7-src/src/*.h");
+            var source = this.CreateCSourceContainer("$(packagedir)/src/regex/2.7/regex-2.7-src/src/regex.c");
+
+            this.PublicPatch((settings, appliedTo) =>
+                {
+                    var compiler = settings as C.ICommonCompilerSettings;
+                    if (null != compiler)
+                    {
+                        compiler.IncludePaths.AddUnique(this.CreateTokenizedString("$(packagedir)/src/regex/2.7/regex-2.7-src/src"));
+                    }
+                });
+
+            source.PrivatePatch(settings =>
+                {
+                    var compiler = settings as C.ICommonCompilerSettings;
+                    compiler.PreprocessorDefines.Add("REGEX_STATIC");
+
+                    if (this.BuildEnvironment.Platform.Includes(Bam.Core.EPlatform.Windows))
+                    {
+                        compiler.PreprocessorDefines.Add("_WIN32");
+                        compiler.PreprocessorDefines.Add("HAVE_STDBOOL_H");
+                    }
+                });
+        }
+    }
 }
