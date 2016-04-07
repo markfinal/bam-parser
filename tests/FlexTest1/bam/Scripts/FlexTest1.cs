36f25947b819dc88d3b5fb15bc9d75cd1a256f66
Mark Final <markfinal@hotmail.com>
2016-04-07 10:49:15 +0100

    [tests,FlexTest1] Fixes for Windows, particularly VisualC which doesn't have a compatible library to link for yywrap, so must use %option noyywrap in the .l file

--- tests/FlexTest1/bam/Scripts/FlexTest1.cs
+++ tests/FlexTest1/bam/Scripts/FlexTest1.cs
@@ -16,30 +16,49 @@ namespace FlexTest1
             var flexInput = Bam.Core.Module.Create<flex.FlexSourceFile>();
             flexInput.InputPath = this.CreateTokenizedString("$(packagedir)/source/scanner.l");
 
-            var enableDebugging = true;
+            var enableDebugging = false;
 
             var flexOutput = source.RunFlex(flexInput, Bam.Core.TokenizedString.CreateVerbatim("yy"), true);
-            flexOutput.Item1.PrivatePatch(settings =>
+            var flexGeneratedSource = flexOutput.Item1; // the .cpp file
+            var flexCompiledGeneratedSource = flexOutput.Item2; // the .obj file
+            flexGeneratedSource.PrivatePatch(settings =>
                 {
                     var flexSettings = settings as flex.IFlexSettings;
                     flexSettings.Debug = enableDebugging;
                     flexSettings.InsertLineDirectives = !enableDebugging;
                 });
-            flexOutput.Item2.PrivatePatch(settings =>
+            flexCompiledGeneratedSource.PrivatePatch(settings =>
                 {
                     var compiler = settings as C.ICommonCompilerSettings;
                     compiler.PreprocessorDefines.Add("D_SCANNER");
+
+                    if (this.Linker is VisualCCommon.LinkerBase)
+                    {
+                        compiler.DisableWarnings.AddUnique("4273"); // lex.yy.cpp(1200): warning C4273: 'isatty' : inconsistent dll linkage
+                    }
+                    else if (this.Linker is MingwCommon.LinkerBase)
+                    {
+                        var cxxCompiler = settings as C.ICxxOnlyCompilerSettings;
+                        cxxCompiler.LanguageStandard = C.Cxx.ELanguageStandard.GnuCxx98; // needed for fileno
+                    }
                 });
 
             this.PrivatePatch(settings =>
                 {
+                    // this is only necessary when %option noyywrap is missing, or yywrap is undefined
+                    // VisualC is the exception, where the provided library with the prebuilt flex is incompatible, and %option noyywrap must be defined
                     var linker = settings as C.ICommonLinkerSettings;
-                    if (null != flexInput.LibrarySearchPath)
+                    if (null != flexGeneratedSource.LibrarySearchPath)
                     {
-                        linker.LibraryPaths.AddUnique(flexInput.LibrarySearchPath);
+                        linker.LibraryPaths.AddUnique(flexGeneratedSource.LibrarySearchPath);
                     }
-                    linker.Libraries.AddUnique(flexInput.StandardLibrary);
+                    linker.Libraries.AddUnique(flexGeneratedSource.StandardLibrary(this.Linker));
                 });
+
+            if (this.Linker is VisualCCommon.LinkerBase)
+            {
+                this.LinkAgainst<WindowsSDK.WindowsSDK>();
+            }
         }
     }
 }
