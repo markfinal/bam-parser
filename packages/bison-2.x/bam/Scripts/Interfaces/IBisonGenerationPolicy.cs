47826628ff6b4f674186e1784a997ac5bee8e900
Mark Final <markfinal@hotmail.com>
2018-08-12 21:48:31 +0100

    Predicating out unused policies

--- packages/bison-2.x/bam/Scripts/Interfaces/IBisonGenerationPolicy.cs
+++ packages/bison-2.x/bam/Scripts/Interfaces/IBisonGenerationPolicy.cs
@@ -27,6 +27,8 @@
 // OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #endregion // License
+#if BAM_V2
+#else
 using Bam.Core;
 namespace bison
 {
@@ -41,3 +43,4 @@ namespace bison
             BisonSourceFile source);
     }
 }
+#endif
